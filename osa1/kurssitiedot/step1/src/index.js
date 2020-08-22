2677b4b9c516c60aac3b96ddaa5e3eb8683c6eff
Carl Sjövall <caersj@utu.fi>
2020-08-23 00:08:53 +0300

    Step3 tehty

--- osa1/kurssitiedot/step1/src/index.js
+++ osa1/kurssitiedot/step1/src/index.js
@@ -3,23 +3,31 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
 
   return (
     <div>
       <Header course={course} />
-      <Content name1={part1} name2={part2} name3={part3} exercise1={exercises1} exercise2={exercises2} exercise3={exercises3} />
-      <Total exercise1={exercises1} exercise2={exercises2} exercise3={exercises3} />
+      <Content name1={part1.name} name2={part2.name} name3={part3.name} exercise1={part1.exercises} exercise2={part2.exercises} exercise3={part3.exercises} />
+      <Total exercise1={part1.exercises} exercise2={part2.exercises} exercise3={part3.exercises} />
     </div>
   )
 }
 
 const Header = (props) => {
+  console.log(props)
   return (
     <div>
     <h1>{props.course}</h1>
@@ -27,6 +35,7 @@ const Header = (props) => {
   )
 }
 const Content = (props) => {
+  console.log(props)
   return (
     <div>
     <Part name ={props.name1} exercise={props.exercise1} />
@@ -37,6 +46,7 @@ const Content = (props) => {
 }
 
 const Total = (props) => {
+  console.log(props)
   return (
     <div>
       <p>
@@ -47,6 +57,7 @@ const Total = (props) => {
 }
 
 const Part = (props) => {
+  console.log(props)
   return (
     <p>{props.name} {props.exercise}</p>
   )
