06e0ba615c87e38848d9dfe0b9da8859f192a0b4
Carl Sjövall <caersj@utu.fi>
2020-08-31 18:43:22 +0300

    counter nappeineen

--- osa1/counter/src/index.js
+++ osa1/counter/src/index.js
@@ -4,16 +4,17 @@ import ReactDOM from 'react-dom';
 const App = (props) => {
   const [ counter, setCounter] = useState(0)
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
-  
-  console.log('rendering...', counter)
-
-
   return (
-    <div>{counter}</div>
+    <div>
+      <div>{counter}</div>
+      <button onClick={() => setCounter(counter + 1)}>
+        plus
+      </button>
+      <button onClick={() => setCounter(0)}>
+        zero
+      </button>
+    </div>
+    
   )
 }
 
