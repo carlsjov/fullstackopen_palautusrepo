d0628501ed2f297fe6d2f8d1c4d93bd43129d6a2
Carl Sjövall <caersj@utu.fi>
2020-08-31 18:59:49 +0300

    osa1bc counter tehty

--- osa1/counter/src/index.js
+++ osa1/counter/src/index.js
@@ -3,18 +3,29 @@ import ReactDOM from 'react-dom';
 
 const App = (props) => {
   const [ counter, setCounter] = useState(0)
+  
+  const increaseByOne = () => setCounter(counter +1)
+  const setToZero = () => setCounter(0)
+  const decreaseByOne = () => setCounter(counter -1)
 
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>
-        plus
-      </button>
-      <button onClick={() => setCounter(0)}>
-        zero
-      </button>
+      <Display counter={counter} />
+      <Button handleClick={increaseByOne} text='plus' />
+      <Button handleClick={decreaseByOne} text='minus' />
+      <Button handleClick={setToZero} text='zero' />
     </div>
     
+  )
+}
+
+const Display = ({counter}) => (<div>{counter}</div>)
+
+const Button = ({handleClick,text}) => {
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
   )
 }
 
